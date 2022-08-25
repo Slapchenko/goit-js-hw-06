8ccf75b100ddfb43d21c9acd35be34dbb4dc6dbf
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-26 00:54:02 +0300

    fix task 3

--- js/task-03.js
+++ js/task-03.js
@@ -31,24 +31,31 @@ galleryRel.style.padding = '0';
 galleryRel.style.listStyle = 'none';
 
 // *v1
-const addImages = images.map(element => {
-  const li = document.createElement('li');
-  li.style.marginBottom = '20px';
-
-  const image = document.createElement('img');
-  image.src = element.url;
-  image.alt = element.alt;
-  image.width = 320;
-  image.height = 212;
-
-  li.appendChild(image);
-
-  return li.outerHTML;
+const createGalleryItemsMarkup = images.map(image => {
+  return `<li><img class = "gallery-item" src = "${image.url}" alt = "${image.alt}" style="width: 320px; height: 212px;"></li>`;
 });
 
-galleryRel.insertAdjacentHTML('beforeend', addImages.join(''));
+galleryRel.insertAdjacentHTML('beforeend', createGalleryItemsMarkup.join(''));
 
 // *v2
+// const addImages = images.map(element => {
+//   const li = document.createElement('li');
+//   li.style.marginBottom = '20px';
+
+//   const image = document.createElement('img');
+//   image.src = element.url;
+//   image.alt = element.alt;
+//   image.width = 320;
+//   image.height = 212;
+
+//   li.appendChild(image);
+
+//   return li.outerHTML;
+// });
+
+// galleryRel.insertAdjacentHTML('beforeend', addImages.join(''));
+
+// *v3
 // for (let i = 0; i < images.length; i += 1) {
 //   const li = document.createElement('li');
 //   li.style.marginBottom = '20px';
